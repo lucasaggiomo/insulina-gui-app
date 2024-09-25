7b5dce9b8abf37bbe579cd8bd1923c18efe2e2af
Luca Saggiomo <luca@paolosaggiomo.it>
2024-09-25 19:08:38 +0200

    aggiunta frontend

---
+++ frontend/App.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        # Size iniziale
+        self.geometry("700x500")
+
+        # Titolo finestra
+        self.title("Fondamenti di Misure")
+
+        # Non permettere il resize
+        # self.resizable(0, 0)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
